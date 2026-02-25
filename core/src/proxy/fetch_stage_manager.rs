7508442a10daefa1620cfa607aff71178662e79c
seg <zano@jito.wtf>
2026-02-25 13:52:17 -0300

    fix test

--- core/src/proxy/fetch_stage_manager.rs
+++ core/src/proxy/fetch_stage_manager.rs
@@ -479,13 +479,13 @@ mod tests {
         expected_fwd_addr: &SocketAddr,
     ) {
         assert_eq!(
-            cluster_info.my_contact_info().tpu(Protocol::UDP).unwrap(),
+            cluster_info.my_contact_info().tpu(Protocol::QUIC).unwrap(),
             *expected_addr
         );
         assert_eq!(
             cluster_info
                 .my_contact_info()
-                .tpu_forwards(Protocol::UDP)
+                .tpu_forwards(Protocol::QUIC)
                 .unwrap(),
             *expected_fwd_addr
         );
